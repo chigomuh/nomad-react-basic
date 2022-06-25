08196777d295022aecb6c0ea20aec05b72f09d96
chigosix <tngusrldnjs1@gmail.com>
2022-06-25 21:49:33 +0900

    내비게이션바 디자인 완성

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,51 @@
 import logo from "../img/Netflix-Brand-Logo.png";
+import searchIcon from "../img/searchIcon.png";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const nav = document.querySelector(".nav");
+
+  window.addEventListener("scroll", () => {
+    if (!nav?.classList?.value.includes("bg-black") && window.scrollY > 0) {
+      nav?.classList.add("bg-black");
+    } else if (
+      window.scrollY <= 0 &&
+      nav?.classList?.value.includes("bg-black")
+    ) {
+      nav?.classList.remove("bg-black");
+    }
+  });
+
   return (
-    <nav className="bg-gray-200">
+    <nav className="fixed w-full nav transition-all duration-700">
       <div className="max-w-6xl mx-auto px-4">
-        <div className="flex justify-between">
+        <div className="flex justify-between relative">
           {/* 왼쪽 메뉴 */}
           <div className="flex items-center">
-            <img className="w-24 h-14 my-1" src={logo} alt="logo" />
-            <div className="flex md:hidden space-x-4 items-center">
-              <div>메뉴</div>
-            </div>
-            <div className="hidden md:flex space-x-4 items-center">
+            <Link to="/">
+              <img className="w-24 h-14 my-1 mr-2" src={logo} alt="logo" />
+            </Link>
+            <label
+              className="md:hidden py-5 px-3 text-zinc-100 transition-all duration-150 hover:text-zinc-300"
+              htmlFor="menuCheckbox"
+            >
+              메뉴
+            </label>
+            <input id="menuCheckbox" type="checkbox" className="peer hidden" />
+            <div className="hidden peer-checked:block peer-checked:absolute peer-checked:top-16 peer-checked:left-[136px] peer-checked:border-8 peer-checked:border-transparent peer-checked:border-b-white peer-checked:border-solid peer-checked:opacity-80"></div>
+            <div className="hidden md:flex space-x-4 items-center peer-checked:block peer-checked:absolute peer-checked:top-20 peer-checked:left-4 peer-checked:text-center peer-checked:w-64 peer-checked:opacity-80 peer-checked:bg-black peer-checked:border-t-2 border-white border-solid">
               <Link to="/">
-                <div className="py-5 px-3 text-gray-700 hover:text-gray-900">
+                <div className="py-5 px-3 text-zinc-100 hover:bg-neutral-900 transition-all duration-150 md:hover:text-zinc-300 md:hover:bg-inherit">
                   홈
                 </div>
               </Link>
               <Link to="/movie/korea">
-                <div className="py-5 px-3 text-gray-700 hover:text-gray-900">
+                <div className="py-5 px-3 text-zinc-100 hover:bg-neutral-900 transition-all duration-150 md:hover:text-zinc-300 md:hover:bg-inherit">
                   국내 영화
                 </div>
               </Link>
               <Link to="/movie/other">
-                <div className="py-5 px-3 text-gray-700 hover:text-gray-900">
+                <div className="py-5 px-3 text-zinc-100 hover:bg-neutral-900 transition-all duration-150 md:hover:text-zinc-300 md:hover:bg-inherit">
                   해외 영화
                 </div>
               </Link>
@@ -40,27 +61,17 @@ function Navbar() {
               />
               <label
                 htmlFor="searchCheckbox"
-                className="text-gray-700 hover:text-gray-900 peer-checked:bg-white transition-all duration-700 rounded-l-md items-center flex pl-2"
+                className="text-zinc-300 transition-all duration-700 rounded-l-md items-center flex pl-2 peer-checked:bg-black peer-checked:border-2 peer-checked:border-r-0 border-solid border-white pr-2"
               >
-                <svg
-                  width="24px"
-                  height="24px"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"
-                  />
-                </svg>
+                <img src={searchIcon} alt="search-icon" className="w-6 h-6" />
               </label>
               <input
                 type="text"
-                className="py-1 w-0 bg-gray-200 peer-checked:w-40 peer-checked:bg-white transition-all duration-700 rounded-r-md"
+                className="py-1 w-0 peer-checked:w-36 bg-black transition-all duration-700 rounded-r-md peer-checked:border-2 border-solid border-white"
               />
             </div>
             <Link to="/login">
-              <div className="py-1 px-3 text-yellow-700 hover:text-yellow-900 bg-yellow-400 hover:bg-yellow-300 rounded transition duration-300">
+              <div className="hidden md:block py-1 px-3 text-yellow-700 hover:text-yellow-900 bg-yellow-400 hover:bg-yellow-300 rounded transition duration-300">
                 로그인
               </div>
             </Link>
